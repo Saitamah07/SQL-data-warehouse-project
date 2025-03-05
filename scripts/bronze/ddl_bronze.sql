7f34f1a8beb4ebf49795e2cc329110a65117b2a7
Matías Alvarez <matias279ah@gmail.com>
2025-03-05 12:22:35 -0500

    Update ddl_bronze.sql
    Fixing the column cst_marital_status and cst_gndr

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -15,13 +15,13 @@ IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.crm_cust_info (
-	cst_id				 INT,
-	cst_key				 NVARCHAR(50), 
-	cst_firstname		 NVARCHAR(50),
-	cst_lastname		 NVARCHAR(50),
-	cst_material_status	 NVARCHAR(50),
-	cst_gdnr			 NVARCHAR(50),
-	cst_create_date		 DATE 
+	cst_id			INT,
+	cst_key			NVARCHAR(50), 
+	cst_firstname		NVARCHAR(50),
+	cst_lastname		NVARCHAR(50),
+	cst_marital_status	NVARCHAR(50),
+	cst_gndr		NVARCHAR(50),
+	cst_create_date		DATE 
 );
 GO
 
@@ -35,7 +35,7 @@ CREATE TABLE bronze.crm_prd_info (
 	prd_nm			NVARCHAR(50),
 	prd_cost		INT,
 	prd_line		NVARCHAR(50),
-	prd_start_dt	DATETIME,
+	prd_start_dt		DATETIME,
 	prd_end_dt		DATETIME
 );
 GO
@@ -49,11 +49,11 @@ CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num		NVARCHAR(50),
 	sls_prd_key		NVARCHAR(50),
 	sls_cust_id		INT,
-	sls_order_dt	INT,
+	sls_order_dt		INT,
 	sls_ship_dt		INT,
 	sls_due_dt		INT,
 	sls_sales		INT,
-	sls_quantity	INT,
+	sls_quantity		INT,
 	sls_price		INT
 
 );
@@ -65,7 +65,7 @@ GO
 
 CREATE TABLE bronze.erp_cust_az12 (
 	cid		NVARCHAR(50),
-	bdate	DATE,
+	bdate		DATE,
 	gen		NVARCHAR(50)
 )
 GO
@@ -78,7 +78,7 @@ GO
 
 CREATE TABLE bronze.erp_loc_a101 (
 	cid		NVARCHAR(50),
-	cntry	NVARCHAR(50)
+	cntry		NVARCHAR(50)
 )
 GO
 
@@ -88,13 +88,9 @@ GO
 
 
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-	id				NVARCHAR(50),
-	cat				NVARCHAR(50),
+	id			NVARCHAR(50),
+	cat			NVARCHAR(50),
 	subcat			NVARCHAR(50),
 	maintenance		NVARCHAR(50)
 )
 GO
-
-
-
-
